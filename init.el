df9291390884fbeb61bd1205c9e4271ad2b05161
Steffen Fiedler <steffen@nand.io>
2017-12-16 13:55:02 +0100

    Removes melpa archive and keeps stable only

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 ;; Initialize package 
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
