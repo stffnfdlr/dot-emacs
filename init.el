d711dfa4dd1c94284db84555ca2ca050a5be2465
Steffen Fiedler <steffen@nand.io>
2018-01-26 11:31:41 +0100

    adds .scss to web-mode hook

--- init.el
+++ init.el
@@ -283,6 +283,7 @@
              (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
              (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
              (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
              (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
              :config
              (setq web-mode-css-indent-offset 2)
