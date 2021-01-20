fe939e6b6c335889f7894761ea5963406587db7f
Steffen Fiedler <steffen@nand.io>
2021-01-20 08:59:18 +0100

    adds org-gcal to retrieve Google Calendar events for org agenda

--- secrets.template.el
+++ secrets.template.el
@@ -4,3 +4,7 @@
 (setq secrets-irc-freenode-user "myNickName")
 (setq secrets-irc-freenode-pass "myPassWord")
 
+(setq
+ secrets-org-gcal-client-id "some-hash-goes-here.apps.googleusercontent.com"
+ secrets-org-gcal-client-secret "my-gcal-google-client-secret"
+ secrets-org-gcal-file-alist '(("your-email@domain.com" . "~/your user/gcal.org")))
