29fa8a7d0f580803eee2d2dff6ae5b672448cf44
Steffen Fiedler <steffen@nand.io>
2026-08-21 20:29:43 +0200

    feat: adopt amx and opt-in tree-sitter modes

--- init.el
+++ init.el
@@ -41,12 +41,12 @@
    '("2ade04230c5a9a82729a33be6e942441942b8532311274b4f8155edcded5b0ca"
      default))
  '(package-selected-packages
-   '(avy cider company counsel csv-mode diff-hl diminish docker dockerfile-mode
+   '(amx avy cider company counsel csv-mode diff-hl diminish docker dockerfile-mode
          eslint-rc exec-path-from-shell expand-region gnuplot
          ledger-mode magit markdown-mode nix-mode notmuch ob-http
          ob-typescript olivetti org-contrib org-journal org-roam
          ox-gfm plantuml-mode projectile rainbow-delimiters restclient
-         smartparens smex svelte-mode typescript-mode web-mode
+         smartparens svelte-mode typescript-mode web-mode
          writegood-mode yaml-mode))
  '(sgml-basic-offset 2))
 (custom-set-faces
